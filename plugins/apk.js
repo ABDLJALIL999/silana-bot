cc0ce0fde5c60ff0645388205c1debbab6eb5a02
ABDLJALIL999 <botw755@gmail.com>
2024-07-25 21:56:22 +0100

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   });
 
   await conn.sendMessage(m.chat, {
-    text: `*جاري تحميل* ${info.name}...\n\nسعيد انك تستعمل بوبيزة بوت وسأكون مسرورا 😄 إن انضممت لأنستغرامي\ninstagram.com/noureddine_ouafy`,
+    text: `*جاري تحميل* ${info.name}...\n\nمرحبا بك في بوت واتساب هدا هو رقم المطور\wa.me/212614932692`,
   });
 
   await conn.sendMessage(
